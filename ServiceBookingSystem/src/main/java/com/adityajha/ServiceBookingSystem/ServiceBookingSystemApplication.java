8d810665df95d74a567682ac5bc40c27c79aae33
git-AdityaJha <adityakrjha111@gmail.com>
2024-04-06 11:46:16 +0530

    First Commit

---
+++ ServiceBookingSystem/src/main/java/com/adityajha/ServiceBookingSystem/ServiceBookingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.adityajha.ServiceBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookingSystemApplication.class, args);
+
+		System.out.println("\nHello world !");
+	}
+}
